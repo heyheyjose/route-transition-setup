a66a5e7cca1fd87f0088b8afdbd38a1ff5312156
Jose Vazquez <josevazquez7@deloitte.com>
2018-09-11 11:04:38 -0400

    added some global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import './global-styles';
 
 import Header from './Header';
 import Container from './Container';
